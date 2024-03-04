f9b6f99d38c1a8cbda6e68f5d294131a8411d220
Levisheshhh <118990266+Levisheshhh@users.noreply.github.com>
2024-03-04 21:40:41 +0800

    initial commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stye.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<style>
+    
+#home,
+#about,
+#project,
+#contact{
+    height: 700px;
+}
+
+    #home{
+    background: linear-gradient(#EBE3D5,#B0A695,#776B5D);
+}
+.header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 20px 10%;
+    background: #F3EEEA;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 100;
+}
+
+.home{
+    height: 100vh;
+    background: linear-gradient(#EBE3D5,#B0A695,#776B5D);
+    color: #FAE7C9;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10%;
+}
+.home-image img{
+    height: 400px;
+    width: 400px;
+    border-radius: 50%;
+    border: 10px solid #fff;
+
+}
+</style>
+<body>
+    <section class="home" id="home">
+        <div class="home-image">
+            <img src="./picture.jpg">
+        </div>
+
+        <div class="home-context">
+            <h1>Hello, I'm Van Levi S. Mangaldan</h1>
+            <p>Greetings! a passionate IT student dedicated to turning ideas into impactful realities. My portfolio is a testament to my journey, showcasing a blend of creativity, innovation, and expertise in IT field. Join me as we explore the intersection of vision and execution.</p>
+            <p></p>
+            <p></p>
+            </div>
+        </div>
+</body>
+</html>
